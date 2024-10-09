4f9e993534f0c18f1961f415f3df3a8b286181ea
h2ryen <107765935+h2ryen@users.noreply.github.com>
2024-10-09 16:38:13 +0800

    Update wanyuanyuedu.js

--- wanyuanyuedu.js
+++ wanyuanyuedu.js
@@ -12,7 +12,7 @@
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/wanyuanyuedu.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/h2ryen/HRQX/refs/heads/imagine/wanyuanyuedu.js
 
 [mitm] 
 
